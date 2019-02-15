e5c687cfb56dd12feff8f596db2595e6cb1ae41f
Henrik Holm <henrik@berakningsbyran.se>
2019-02-15 22:00:58 +0100

    Added use of images in Fluke gui

--- python/components/flukegui.py
+++ python/components/flukegui.py
@@ -3,6 +3,7 @@
 
 import time
 import os
+import math as m
 
 import pygame
 import pygame.gfxdraw
@@ -28,37 +29,35 @@ class FlukeGUI(BaseGUI):
 
 	def draw_battery(self):
 
-		fill_ratio = self.objects[0].dc_capacitor_voltage / 170.0
+		image_index = int((self.objects[0].dc_capacitor_voltage / 150.0)*5) + 1
 
-		color_outline = (255, 255, 255)
-		if fill_ratio < 0.2:
-			color_fill = (255, 0, 0)
-		else:
-			color_fill = (255, 255, 255)
+		imagefile = "battery_%d.png" % image_index
+		segment = pygame.image.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'images', imagefile))
+		segment = segment.convert_alpha()
+		self.screen.blit(segment, (0,0))
 
-		x = 683
-		y = 90
-		width = 100
-		height = 240
-		line_width = 2
 
-		spacing = 1
+	def draw_power(self):
 
-		# Draw battery fill level
-		loffset = spacing + line_width
-		roffset = spacing + 2 * line_width
-		fill_max_height = (height - roffset)
-		fill_height = fill_max_height * fill_ratio
-		fill_width = width - roffset
-
-		pygame.draw.rect(self.screen, color_fill,
-				[x + loffset, y + loffset + fill_max_height * (1.0 - fill_ratio), fill_width, fill_height])
+		power = max(self.objects[1].motor_power, 0)
+		needle = pygame.image.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'images', 'kw_needle.png'))
+		needle = needle.convert_alpha()
+		needle = pygame.transform.rotozoom(needle, -power*(160.0/70.0), 1.0)
+		pos = needle.get_rect()
+		pos.centerx = 503
+		pos.centery = 189
+		self.screen.blit(needle, pos)
 
 	def draw_speed(self):
 
 		speed = self.objects[0].get_speed()
-
-		self.draw_shadow_text(self.screen, "%.0f" % speed, 100, FONT_NAME, (480, 120), True)
+		needle = pygame.image.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'images', 'speed_needle.png'))
+		needle = needle.convert_alpha()
+		needle = pygame.transform.rotozoom(needle, -speed*(180.0/160.0), 1.0)
+		pos = needle.get_rect()
+		pos.centerx = 399
+		pos.centery = 187
+		self.screen.blit(needle, pos)
 
 	def draw_background(self):
 		bg = pygame.image.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'images', 'background.png'))
@@ -66,16 +65,81 @@ class FlukeGUI(BaseGUI):
 		bg = pygame.transform.scale(bg, (800, 480))
 		self.screen.blit(bg, (0,0))
 
+	def load_full_image(self, filename):
+			img = pygame.image.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'images', filename))
+			img = img.convert_alpha()
+			self.screen.blit(img, (0, 0))
+
+	def set_highbeam(self, state):
+		if state:
+			self.load_full_image('high_beam.png')
+
+	def set_turn_left(self, state):
+		if state:
+			self.load_full_image('left_turn.png')
+
+	def set_turn_right(self, state):
+		if state:
+			self.load_full_image('right_turn.png')
+
+	def set_warning_signal(self, state):
+		if state:
+			self.load_full_image('warn_signal.png')
+
+	def print_rpm(self):
+		rpm = max(self.objects[0].actual_speed, 0)
+		self.draw_text(self.screen, str(rpm), 60, (20, 217))
+
+	def print_power(self):
+		power = max(self.objects[1].motor_power, 0)
+		self.draw_text(self.screen, str(power), 60, (20, 325))
+
+	def print_current(self):
+		current = max(self.objects[0].motor_rms_current, 0)
+		self.draw_text(self.screen, str(current), 60, (20, 107))
+
+	def print_battery_voltage(self):
+		voltage = max(self.objects[0].dc_capacitor_voltage, 0)
+		self.draw_text(self.screen, "%.0f" % voltage, 30, (590, 109))
+
+	def print_range(self):
+		range = max(self.objects[2].odometer, 0)
+		self.draw_text(self.screen, "%.0f" % range, 60, (635, 322), topright=True)
+
+	def print_odometer(self):
+		odometer = max(self.objects[2].odometer, 0)
+		self.draw_text(self.screen, "%.0f" % odometer, 45, (754, 430), topright=True)
+
+	def print_ctrl_temp(self):
+		temp = max(self.objects[1].controller_temp, 0)
+		self.draw_text(self.screen, "%.0f" % temp, 45, (527, 430), topright=True)
+
+	def print_motor_temp(self):
+		temp = max(self.objects[1].motor_temp, 0)
+		self.draw_text(self.screen, "%.0f" % temp, 45, (304, 430), topright=True)
+
 	def run(self):
+		count = 0
 		while not self.shutdown.is_set():
 			self.draw_background()
 			self.draw_speed()
 			self.draw_battery()
-			self.draw_gauge(240, 380, 40, "Ctrl. Temp", self.objects[1].controller_temp, "°C")
-			self.draw_gauge(340, 380, 40, "RMS Amps", self.objects[0].motor_rms_current, "A")
-			self.draw_gauge(460, 380, 60, "Power", self.objects[1].motor_power, "kW")
-			self.draw_gauge(580, 380, 40, "Batt. Amps", self.objects[0].battery_current, "A")
-			self.draw_gauge(680, 380, 40, "Motor Temp", self.objects[1].motor_temp, "°C")
+			self.draw_power()
+			self.set_highbeam(count%10==0)
+			self.set_turn_left(count%10==2)
+			self.set_turn_right(count%10==4)
+			self.set_warning_signal(count%10==6)
+
+			self.print_rpm()
+			self.print_power()
+			self.print_current()
+			self.print_battery_voltage()
+			self.print_range()
+			self.print_odometer()
+			self.print_ctrl_temp()
+			self.print_motor_temp()
+
+			count += 1
 
 			pygame.display.flip()
 
