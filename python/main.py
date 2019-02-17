6bcced3f1cadaaa4bf8a578b8a168346ecb954d8
Henrik Holm <henrik@berakningsbyran.se>
2019-02-17 21:40:51 +0100

    Added eventhandler and profiling

--- python/main.py
+++ python/main.py
@@ -8,17 +8,15 @@ import signal
 import argparse
 import time
 
-import pygame
-import pygame.gfxdraw
-
 from threading import Event
 
 from components.canreader import CanReader
+from components.eventhandler import EventHandler
 from components.messages import *
 from components.megagui import MegaGUI
 from components.flukegui import FlukeGUI
 
-screen = None
+# Global semaphore that makes all threads quit
 shutdown = Event()
 
 
@@ -30,26 +28,39 @@ def run(infile, replay_mode, fullscreen, use_mega):
 	c = CanReader(objects, infile, shutdown, replay_mode)
 	c.start()
 
+	evh = EventHandler(objects, shutdown)
+	evh.start()
+
 	if use_mega:
-		gui = MegaGUI(objects, shutdown, fullscreen)
+		gui = MegaGUI(objects, evh, shutdown, fullscreen)
 	else:
-		gui = FlukeGUI(objects, shutdown, fullscreen)
+		gui = FlukeGUI(objects, evh, shutdown, fullscreen)
 
 	gui.start()
 
+	# Let the main sleep until everyone has acknowledged the shutdown
 	while not shutdown.is_set():
 		time.sleep(0.1)
 
 	c.join()
+	evh.join()
 	gui.join()
+
+
+def run_profile(infile, replay_mode, fullscreen, use_mega):
+	import cProfile
+	pr = cProfile.Profile()
+	pr.enable()
+	run(sys.stdin, args.run_replay, args.use_fullscreen, args.use_mega)
+	pr.disable()
+	pr.print_stats(sort="calls")
+
 
 
 def exit_handler(sig, frame):
 	global shutdown
 	print("Caught Ctrl-C, will exit")
 	shutdown.set()
-	pygame.mouse.set_visible(True)
-	sys.exit(1)
 
 
 if __name__ == "__main__":
@@ -71,3 +82,5 @@ if __name__ == "__main__":
 	fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
 
 	run(sys.stdin, args.run_replay, args.use_fullscreen, args.use_mega)
+	#run_profile(sys.stdin, args.run_replay, args.use_fullscreen, args.use_mega)
+
