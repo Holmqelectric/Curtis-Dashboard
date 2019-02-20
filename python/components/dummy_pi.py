7de8ee73622ace3d2246dcf873f691d5dfd89d70
Henrik Holm <henrik@berakningsbyran.se>
2019-02-20 21:53:04 +0100

    Added some more stuff to dummy GPIO

--- python/components/dummy_pi.py
+++ python/components/dummy_pi.py
@@ -17,13 +17,29 @@ class GPIO(object):
 
 	@classmethod
 	def setup(cls, pin, direction):
-		cls.__pins[pin] = (direction, None)
+		if cls.__pins[pin][0] is not None:
+			print("WARNING: Pin %d is already setup. Has direction '%s'" % (pin, cls.__pins[pin][0]))
+			return
+
+		cls.__pins[pin] = (direction, GPIO.LOW)
 
 	@classmethod
 	def output(cls, pin, state):
 		current_state = cls.__pins[pin]
+		if current_state[0] != GPIO.OUT:
+			print("WARNING: Pin %d is not an output pin." % (pin))
+			#return
+
 		print("Changed pin %d from '%s' to '%s'" % (pin, current_state[1], state))
 		cls.__pins[pin] = (cls.__pins[pin][0], state)
+
+	@classmethod
+	def input(cls, pin):
+		if cls.__pins[pin][0] != GPIO.IN:
+			print("WARNING: Pin %d is not an input pin." % (pin))
+			return None
+		#print("Read state '%s' from pin %d" % (cls.__pins[pin][1], pin))
+		return cls.__pins[pin][1]
 
 	@classmethod
 	def cleanup(cls):
