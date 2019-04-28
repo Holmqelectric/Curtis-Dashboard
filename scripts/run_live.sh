d36161ce6c6fa6cccfdbc5529a432b436f72f280
Henrik Holm <henrik@berakningsbyran.se>
2019-04-28 22:39:13 +0200

    Make script globally runnable

--- scripts/run_live.sh
+++ scripts/run_live.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+runpath="$(dirname $0)/../python/main.py -f"
 fname="/tmp/$(date --iso-8601=seconds).txt"
-candump -L can0 | tee $fname | python3 ../python/main.py
+candump -L can0 | tee $fname | python3 $runpath
